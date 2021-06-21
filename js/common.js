4a5db313724a5d8b8b61f94a9ee457aa73265a63
GAYOUNG914 <rkdud91455@gmail.com>
2021-06-22 01:18:15 +0900

    docs: add codes
    add codes for interactive page

---
+++ js/common.js
@@ -0,0 +1,19 @@
+$(function () {
+  $(window).on("mousemove", mouseMove);
+  function mouseMove(e) {
+    let position = (e.pageX - $(window).width() / 2) * 0.1;
+    //let positionSlow = (e.pageX - $(window).width() / 2) * 0.1;
+    //let positionFast = (e.pageX - $(window).width() / 2) * 0.15;
+    //마우스의 페이지 x값에서 반 나눈 윈도우 너비값을 뺄거임
+    //원점을 잡기 위한 방법
+
+    //console.log(position);
+
+    //$(".spanSlow").css({ transform: "translateX(" + position + "px)" });
+    //$(".spanFast").css({ transform: "translateX(" + -position + "px)" });
+    //gsap.to(".spanSlow", { duration: 0.4, x: positionSlow });
+    //gsap.to(".spanFast", { duration: 0.4, x: -positionFast });
+    gsap.to(".spanSlow", { duration: 0.4, x: position });
+    gsap.to(".spanFast", { duration: 0.4, x: -position });
+  }
+});
